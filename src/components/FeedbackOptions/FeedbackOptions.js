d8a1735a1bc43556b1db5c4d02eed2542d3902c7
opoberezh <opoberezh.2015@gmail.com>
2023-08-11 18:54:22 +0200

    icons

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,10 +1,14 @@
-
+import { ImSmile, ImSad, ImNeutral } from "react-icons/im";
 export const FeedbackOptions =({options, onLeaveFeedback})=>{
+    const icons={
+        good: <ImSmile/>,
+        bad: <ImSad/>,
+        neutral: <ImNeutral/>
+    }
 return(
     <>
         {options.map(option => (
-        <button key={option} onClick={() => onLeaveFeedback(option)}>
-          {option}
+        <button key={option} onClick={() => onLeaveFeedback(option)}>{icons[option]}
         </button>
       ))}
     </>
