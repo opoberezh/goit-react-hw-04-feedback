a6a42e31998b4f6fadd2d3b1777ed9adbe4da7be
opoberezh <opoberezh.2015@gmail.com>
2023-08-11 15:49:21 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import { Component } from "react"
+import { GlobalStyle } from './GlobalStyle';
+
+
+
+export class App extends Component  {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleFeedback = type => {
+    this.setState(prevState =>{
+      return{
+        [type]: prevState[type] +1
+      }
+    })
+  }
+
+  countTotalFeedback(){
+    const  {good, neutral, bad} = this.state
+    return good + neutral + bad
+  }
+
+  countPositivFeedbackPercentage(){
+    const {good} = this.state
+    const totalFeedback = this.countTotalFeedback()
+    return totalFeedback === 0 ? 0 : Math.round(good/totalFeedback * 100)
+  }
+  render(){
+    const {good, neutral, bad} = this.state
+    const totalFeedback = this.countTotalFeedback()
+    const positivFeedbackPercentage = this. countPositivFeedbackPercentage()
+     return (
+    <>
+    <div>
+      <h2>Please leave feedback</h2>
+      <button onClick={() => this.handleFeedback('good')}>Good</button>
+      <button onClick={() => this.handleFeedback('neutral')}>Neutral</button>
+      <button onClick={() => this.handleFeedback('bad')}>Bad</button>
     </div>
+    <div>
+      <h2>Statistic</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad} </p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positiv Feedback: {positivFeedbackPercentage}%</p>
+    </div>
+    <GlobalStyle />
+    </>
+    
+  
   );
+  }
+ 
 };
